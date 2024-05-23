4a6d328ae737efcb785ad81f65834e859ff96062
nadir-0000 <nadirelmesmoudi@gmail.com>
2024-05-23 14:23:27 +0100

    My first commit

---
+++ src/main/java/com/executablejar/ExecutableJarExample.java
@@ -0,0 +1,20 @@
+package com.executablejar;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Main class for Maven Executable Jar.
+ */
+public class ExecutableJarExample {
+
+    public static final String JAVA_REPOSITORY_TEMPLATE = "executable-jar-example";
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour et bon courage dans votre examen en DevOps");
+        // added if to use library from classpath to be sure
+        // that jar gets dependencies
+        if(StringUtils.isNoneEmpty(JAVA_REPOSITORY_TEMPLATE)) {
+            System.out.println(String.format("Hello world, %s!",JAVA_REPOSITORY_TEMPLATE));
+        }
+    }
+}
